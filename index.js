c4c2951ee9fdec61b8229a2e4fc673fb45ee9c1a
Ankit Singh Chauhan <88767069+ankitsingh2105@users.noreply.github.com>
2022-11-14 18:23:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let colorArray = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "pink",
+  "paleturquoise",
+  "brown",
+  "black",
+  "violet",
+  "indigo",
+  "yellow"
+];
+
+let color = document.querySelector(".color");
+console.log(color);
+for (let i = 0; i < 10; i++) {
+  color.innerHTML += `<li style="background : ${colorArray[i]}  " class="${colorArray[i]} inColor">
+     <div class="align" >
+        <div></div>
+        <div>${colorArray[i]}</div>
+     </div> </li> `;
+}
+let incolor = document.querySelectorAll(".inColor");
+
+for (let index = 0; index < 10; index++) {
+  incolor[index].addEventListener("click", function () {
+    document.body.style.background = colorArray[index];
+    localStorage.setItem("notes", colorArray[index]);
+  });
+}
+
+let notes = localStorage.getItem("notes");
+
+if (notes != null) {
+  document.body.style.background = notes;
+}
